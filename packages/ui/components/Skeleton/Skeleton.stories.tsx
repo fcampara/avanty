ad45fed171135ff9682d3e58feda7f0c006c0f61
Felipe Campara Silva <fcamparasilva@gmail.com>
2022-04-22 14:16:25 -0300

    feat(skeleton): create component skeleton

---
+++ packages/ui/components/Skeleton/Skeleton.stories.tsx
@@ -0,0 +1,19 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { Skeleton } from "./styles";
+
+export default {
+  title: "Example/Skeleton",
+  component: Skeleton,
+} as ComponentMeta<typeof Skeleton>;
+
+const Template: ComponentStory<typeof Skeleton> = (args) => (
+  <Skeleton {...args} />
+);
+
+export const Primary = Template.bind({});
+Primary.args = {
+  style: {
+    height: 40,
+    width: 140,
+  },
+};
