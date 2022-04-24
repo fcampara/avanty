efdec0db067646dbb19a6d85179a55982f6590c4
Felipe Campara Silva <fcamparasilva@gmail.com>
2022-04-24 13:15:20 -0300

    fix(link): align icon in center

--- packages/ui/components/Link/styles.ts
+++ packages/ui/components/Link/styles.ts
@@ -2,15 +2,26 @@ import styled from "@emotion/styled"
 
 export const Link = styled.a`
   font-style: normal;
-  font-size: 13px;
+  font-size: 14px;
   text-align: center;
   line-height: 18px;
   letter-spacing: -0.01em;
   cursor: pointer;
   position: relative;
+  display: flex;
+  align-items: center;
+  grid-gap: 2px;
   color: ${({ theme }) => theme.colors.primary.medium};
   font-weight: ${({ theme }) => theme.typography.weight.regular};
   transition: ${({ theme }) => `color ${theme.motion.velocity.fast}`};
+
+  & > svg > path {
+    fill: ${({ theme }) => theme.colors.primary.medium};
+
+    &:hover {
+      fill: ${({ theme }) => theme.colors.accent.medium};
+    }
+  }
 
   &:hover {
     color: ${({ theme }) => theme.colors.accent.medium};
