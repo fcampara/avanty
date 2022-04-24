a046a3dadaffc03cd0412d45bd0e4b5bd1588e48
Felipe Campara Silva <fcamparasilva@gmail.com>
2022-04-23 22:39:47 -0300

    feat(icons): add new icons and fix properties

--- packages/icons/components/ChevronDown.tsx
+++ packages/icons/components/ChevronDown.tsx
@@ -3,8 +3,10 @@ import { IconSVG } from "../types/icons"
 
 const ChevronDrown: ForwardRefRenderFunction<SVGElement, IconSVG> = (
   props: IconSVG,
+  ref,
 ) => (
   <svg
+    ref={ref}
     width="16"
     height="16"
     viewBox="0 0 16 16"
@@ -14,8 +16,8 @@ const ChevronDrown: ForwardRefRenderFunction<SVGElement, IconSVG> = (
   >
     <path
       opacity="0.3"
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M8 9.49112L11.4026 6.18306C11.6536 5.93898 12.0607 5.93898 12.3117 6.18306C12.5628 6.42714 12.5628 6.82286 12.3117 7.06694L8.45457 10.8169C8.20352 11.061 7.79648 11.061 7.54543 10.8169L3.68829 7.06694C3.43724 6.82286 3.43724 6.42714 3.68829 6.18306C3.93934 5.93898 4.34637 5.93898 4.59743 6.18306L8 9.49112Z"
       fill="#022B54"
     />
