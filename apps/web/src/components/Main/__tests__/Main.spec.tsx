abb65e5793dc9286ae7a8ad290c139d87a3c1f3f
Felipe Campara Silva <fcamparasilva@gmail.com>
2022-05-03 09:01:19 -0300

    test(Main): create test component

---
+++ apps/web/src/components/Main/__tests__/Main.spec.tsx
@@ -0,0 +1,21 @@
+import { render } from "@testing-library/react"
+import { HTMLAttributes } from "react"
+import { ThemeProvider } from "ui/styles"
+import { Main } from "../styles"
+
+const dataTestId = "main-testid"
+const Wrapper = (props: HTMLAttributes<HTMLElement>) => {
+  return (
+    <ThemeProvider>
+      <Main data-testid={dataTestId} {...props}/>
+    </ThemeProvider>
+  )
+}
+
+describe("Components/Main", () => {
+  it("Must render normally", () => {
+    const { getByTestId } = render(<Wrapper/>)
+    const element = getByTestId(dataTestId)
+    expect(element).toBeInTheDocument()
+  })
+})
